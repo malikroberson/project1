8793fe1e24d5f7cc3129333314db5e2e770588d5
Malik Roberson <malikroberson@gmail.com>
2021-03-16 12:37:35 -0500

    Submit button functionality

---
+++ js/script.js
@@ -0,0 +1,9 @@
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
+form.onsubmit = submit;
+
+function submit(event) {
+  success.removeAttribute('hidden');
